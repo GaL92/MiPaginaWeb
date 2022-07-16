00115a47ea23398c99e71c58a74cb98b1c8e42ef
Gabi77 <lucerogabi92@gmail.com>
2022-07-16 02:40:34 -0300

    ordenando carpetas en Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-
-import {ButtonComponent} from './components/button/button.component';
-import { EditarComponent } from './components/editar/editar.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import{SocialComponent} from './components/social/social.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { LogoApComponent } from './components/logo-ap/logo-ap.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import {ExperienciaComponent} from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ButtonComponent,
-    EditarComponent,
-    NavbarComponent,
+    SocialComponent,
+    BannerComponent,
+    LogoApComponent,
+    AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    
+    
    
   ],
   imports: [
